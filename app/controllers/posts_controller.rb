14ec047d3a64b06226b1b35a7c1ecd19a63cfd90
Jimmy Lindsey <bkjimmie@gmail.com>
2017-05-21 18:24:53 -0700

    open link in new tab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 30)
+    @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 3)
     
     respond_to do |format|
       format.html
