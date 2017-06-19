92bac98edc164d7af53dd321bcb5ff71fa148367
Jimmy Lindsey <bkjimmie@gmail.com>
2017-06-19 08:35:26 -0700

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -83,10 +83,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   # Required for Heroku
   # Note to set this to your actual host
-  
+
   # S3 for Image Hosting
   config.paperclip_defaults = {
   storage: :s3,
